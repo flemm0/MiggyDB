d8230419ddb980e70895a99cd374d978cd5d0d6e
Flemming Wu <flemming.wu@gmail.com>
2023-10-04 21:47:04 -0700

    saving progress on insertion functions

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,17 @@
 import pyarrow as pa
 import pyarrow.parquet as pq
+import pyarrow.dataset as ds
 from pyarrow import csv, json
 import os
 from subprocess import check_output
 import re
+import subprocess
 
 import polars as pl
 import math
 
 DATA_PATH = '/home/flemm0/school_stuff/USC_Fall_2023/DSCI551-Final_Project/data/'
-TEST_DB_PATH = os.path.join(DATA_PATH, 'test2')
+TEST_DB_PATH = os.path.join(DATA_PATH, 'test')
 
 MAX_PARTITION_SIZE = 100 * 1024 * 1024
 
@@ -20,7 +22,7 @@ def say_hi():
 
 def get_all_partitions(database, table_name):
     '''Returns list of all partitions of a table'''
-    files = os.listdir(os.path.join(DATA_PATH, database))
+    files = os.listdir(os.path.join(DATA_PATH, database, table_name))
     pattern = f'^{table_name}_\d+\.parquet$'
     return [f for f in files if re.match(pattern, f)]
 
@@ -30,8 +32,14 @@ def get_all_partitions(database, table_name):
 def wc(path):
     return int(check_output(['wc', '-l', path]).split()[0])
 
-def create_table_from_csv(path):
+def create_table_from_csv(path, database, table_name=None):
     '''Create a new table in the database system from input csv file. Processes it in 100 MB chunks'''
+    if table_name is None:
+        table_name = os.path.splitext(os.path.basename(path))[0]
+    table_path = os.path.join(DATA_PATH, database, table_name)
+    if not os.path.exists(table_path):
+        os.makedirs(table_path)
+
     total_rows = wc(path)
     n_partitions = math.ceil(os.path.getsize(path) / (1024 ** 2) / 100) ## TODO verify this makes 100 MB chunks
     n_rows = int(total_rows / n_partitions)
@@ -39,14 +47,31 @@ def create_table_from_csv(path):
     for i in range(0, n_partitions):
         skip_rows_after_header = n_rows * i
         data = pl.read_csv(path, n_rows=n_rows, skip_rows_after_header=skip_rows_after_header)
-        fname = os.path.splitext(os.path.basename(path))[0] + '_' + str(i) + '.parquet'
-        data.write_parquet(os.path.join(TEST_DB_PATH, fname))
+        fname = table_name + '_' + str(i) + '.parquet'
+        data.write_parquet(os.path.join(table_path, fname))
 
-def create_table_from_json(path):
+def create_table_from_json(path, database, table_name=None):
     '''Create a new table in the database system from input json file'''
-    data = json.read_json(path)
-    fname = os.path.splitext(os.path.basename(path))[0]
-    pq.write_table(data, os.path.join(TEST_TABLE_PATH, fname + '.parquet'))
+    # check if JSON file is newline-delimited:
+    if wc(path) == 0:
+        try:
+            subprocess.run(f'cat {path} | jq -c \'.[]\' > {path}', shell=True, check=True) # overwrite file with newline-delimited file
+        except subprocess.CalledProcessError as e:
+            print(f'Command failed with exit code: {e.returncode}')
+    if table_name is None:
+        table_name = os.path.splitext(os.path.basename(path))[0]
+    table_path = os.path.join(DATA_PATH, database, table_name)
+    if not os.path.exists(table_path):
+        os.makedirs(table_path)
+
+    total_rows = wc(path)
+    n_partitions = math.ceil(os.path.getsize(path) / (1024 ** 2) / 100)
+    n_rows = int(total_rows / n_partitions)
+
+    for i in range(0, n_partitions):
+        data = pl.scan_ndjson(path, batch_size=n_rows)
+        fname = table_name + '_' + str(i) + '.parquet'
+        data.write_parquet(os.path.join(table_path, fname))
 
 def infer_datatypes(type):
     '''Returns Polars datatype for creating table schema'''
@@ -70,9 +95,12 @@ def create_table_from_cli(database, table_name, schema, primary_key=None, partit
     '''Create new table with schema defined in cli input
     Schema should be a list of tuples
     '''
+    table_path = os.path.join(DATA_PATH, database, table_name)
+    if not os.path.exists(table_path):
+        os.makedirs(table_path)
     data = pl.DataFrame([], schema=schema)
     partition = '_' + str(partition)
-    data.write_parquet(os.path.join(DATA_PATH, database, table_name + partition + '.parquet'))
+    data.write_parquet(os.path.join(table_path, table_name + partition + '.parquet'))
 
 def check_latest_data_partition_size(database, table_name):
     '''Checks most recent parquet file partition size.'''
