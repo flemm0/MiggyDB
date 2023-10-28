8990ff97f62651c61da7d4c185a507b036425823
Flemming <flemming.wu@gmail.com>
2023-10-28 13:20:27 -0700

    successfully implemented external merge sort, cleaning up testing ipynb file

--- testing/external_merge_sort_testing.py
+++ testing/external_merge_sort_testing.py
@@ -53,13 +53,12 @@ sort_col = 'age'
 
 def partial_sort(prev_step_path, sort_col):
     dataset = ds.dataset(prev_step_path, format='parquet')
+    sort_idx = dataset.schema.names.index(sort_col)
 
     for partition in dataset.files:
         partition = Path(partition)
-        data = pl.read_parquet(partition)
-        idx = data.columns.index(sort_col)
-        data = data.rows()
-        data.sort(key=lambda x: x[idx])
+        data = pl.read_parquet(partition).rows()
+        data.sort(key=lambda x: x[sort_idx])
         data = pl.DataFrame(data, schema=list(pl.read_parquet_schema(partition).keys())).to_arrow()
         yield data, partition.stem
 
@@ -76,45 +75,72 @@ for partition, name in partial_sort('./step_1/', 'age'):
 
 def merge_sorted_runs(prev_step_path, sort_col):
     dataset = ds.dataset(prev_step_path, format='parquet')
+    sort_idx = dataset.schema.names.index(sort_col)
     n_buffers = len(dataset.files) + 1 # add 1 for output buffer
+    out_buffer_len = dataset.count_rows() // len(dataset.files)
 
     # create iterators of each of the files
-    iterators = {}
+    chunk_iterators = {}
     for partition in dataset.files:
         pf = pq.ParquetFile(partition)
         num_rows = pf.metadata.num_rows
-        iterators[Path(partition).stem] = pf.iter_batches(batch_size=num_rows // n_buffers)
-
+        chunk_iterators[Path(partition).stem] = pf.iter_batches(batch_size=num_rows // n_buffers)
+    out_partition_names = list(chunk_iterators.keys())
     # store chunks of iterators in dictionary
-    chunks = {}
-    for name, iterator in iterators.items():
-        chunks[name] = next(iterator, False)
+    chunks = {
+        name: next(chunk_iterator, False) for name, chunk_iterator in chunk_iterators.items()
+    }
 
-    # create tuple iterators for individual rows
-    # TODO change to use dictionary like the chunk iterator
-    row_iterators = [iter([tuple(r.values()) for r in c.to_pylist()]) for c in chunks.values()]
-    rows = [next(row_iterator, None) for row_iterator in row_iterators] # list of tuples, 1 for each chunk
-    merged_data = []
+    # create tuple iterators for individual rows 
+    row_iterators = {
+        name: iter([tuple(t.values()) for t in chunk.to_pylist()]) for name, chunk in chunks.items()
+    }
 
+    # dict of tuples, 1 for each chunk
+    rows = { 
+        name: next(row_iterator, None) for name, row_iterator in row_iterators.items()
+     } 
+    
+    merged_data = [] # list to hold output buffer
+    out_partition_counter = 0
     while any(chunks.values()):
-        min_tuple = min(rows, key=lambda x: x[1])
-        min_index = rows.index(min_tuple)
+        min_tuple = min(rows.values(), key=lambda x: x[sort_idx])
+        min_chunk = list(filter(lambda x: rows[x] == min_tuple, rows))[0]
         merged_data.append(min_tuple)
-        # if len(merged_data) > min([len(chunk) for chunk in chunks.values]):
-        #   write_table()
-        rows[min_index] = next(row_iterators[min_index], None)
-        if not rows[min_index]:
-            pass
-            # call `next()` on the chunk to load next chunk
+        # write to disk if output buffer is full
+        if len(merged_data) > out_buffer_len:
+          yield pl.DataFrame(merged_data, schema=dataset.schema.names).to_arrow(), out_partition_names[out_partition_counter]
+          out_partition_counter += 1
+          merged_data = []
+        rows[min_chunk] = next(row_iterators[min_chunk], None)
+        if not rows[min_chunk]:
+            # update chunks dict, load next chunk
+            chunks[min_chunk] = next(chunk_iterators[min_chunk], False)
+            # if still chunks to iterate, load nexxt
+            if chunks[min_chunk] != False:
+                # update row_iterators dict - create new tuple iterator
+                row_iterators[min_chunk] = iter([tuple(t.values()) for t in chunks[min_chunk].to_pylist()])
+                # update rows_dict
+                rows[min_chunk] = next(row_iterators[min_chunk], False)
+            # if no more chunks, remove from dict
+            else:
+                del rows[min_chunk]
 
-    # add logic to check size of merged dataset, and write to disk once reached
+    yield pl.DataFrame(merged_data, schema=dataset.schema.names).to_arrow(), out_partition_names[out_partition_counter]
 
-    print(merged_data)
-# %%
+for chunk, name in merge_sorted_runs(Path(base / 'step_2'), 'age'):
+    pq.write_table(table=partition, where=(base / 'step_3' / name).with_suffix('.parquet'))
 
+#%%
 # check
-dataset = ds.dataset(base / "step_2", format="parquet")
+dataset1 = ds.dataset(base / "step_1", format="parquet")
+table1 = dataset1
+#table1.take([0,1,2,3])
 
-dataset.to_table().sort_by('age').to_pandas().head(10)
+dataset2 = ds.dataset(base / 'step_3', format='parquet')
+table2 = dataset2.to_table()
 
+table2
+# %%
+pl.DataFrame._from_arrow(table2)
 # %%
