ecf27584432cd3215d734c624315eb0981771ae2
Flemming Wu <flemming.wu@gmail.com>
2023-11-07 11:31:47 -0800

    add cli script to entrypoint for access with command

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@ with open("requirements.txt", encoding="utf-8") as f:
     required = f.read().splitlines()
 
 setup(
-    name='DSCI 551 Database',
+    name='miggydb',
     version='0.1',
     description='Custom Database System',
     author='Flemming Wu',
     author_email='wuflemmi@usc.edu',
     packages=find_packages(),
+    entry_points = {
+        'console_scripts': ['miggydb = src.cli:main',],
+    },
     install_requires=required,
     python_requires=">=3.10"
 )
