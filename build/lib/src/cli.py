7530e71daccec3ede28b8b794e965a9cc825c9e0
Flemming Wu <flemming.wu@gmail.com>
2023-11-07 13:39:13 -0800

    editing cli parsers to accept commands without underscores

--- build/lib/src/cli.py
+++ build/lib/src/cli.py
@@ -1,4 +1,4 @@
-import cmd
+from cmd2 import Cmd
 import os
 from pathlib import Path
 import polars as pl
@@ -6,8 +6,15 @@ import polars as pl
 import utils
 
 data_dir = Path('/home/flemm0/school_stuff/USC_Fall_2023/DSCI551-Final_Project/data/')
+'''
+TODO set default data dir
 
-class DatabaseCLI(cmd.Cmd):
+/var/lib/miggydb for linux
+/usr/local/var/miggydb for mac
+'''
+
+
+class DatabaseCLI(Cmd):
     prompt = '\U0001F911 > '
 
     def __init__(self):
@@ -18,30 +25,47 @@ class DatabaseCLI(cmd.Cmd):
         self.current_db = None
         self.tables = None
 
-    def do_show_dbs(self, arg):
-        '''Prints all available databases under the data directory'''
-        print('Existing databases:\n')
-        print(*self.dbs, sep='\n')
-
-    def do_use_db(self, arg):
-        '''Sets the path to the database to be queried. Loads tables names from database into memory.'''
-        db_name = arg.strip()
-        db_path = data_dir / db_name
-
-        if os.path.exists(db_path):
-            self.current_db = db_path
-            self.tables = [dir.stem for dir in list(db_path.iterdir())]
-            print(f'Using database: {db_name}')
+    def do_new(self, arg):
+        '''argument parser for creating a new database'''
+        args = arg.split()
+        if args[0] in ['db', 'database']:
+            Path.mkdir(data_dir / args[1])
+            if (data_dir / args[1]).exists():
+                print(f'Database {args[1]} successfully created')
         else:
-            print(f'Database {db_name} not found')
+            print('Unrecognized command')
 
-    def do_show_existing_tables(self, arg):
-        if self.current_db is not None:
-            print('Existing tables:\n')
-            print(*self.tables, sep='\n')
+    def do_show(self, arg):
+        '''argument parser for viewing existing databases and tables'''
+        if arg in ['dbs', 'databases']:
+            print('Existing databases:\n')
+            print(*self.dbs, sep='\n')
+        elif arg == 'tables':
+            if self.current_db is not None:
+                print(f'Existing tables under {Path(self.current_db).stem}:\n')
+                print(*self.tables, sep='\n')
+            else:
+                print(f'Database not set. Please set database to use to view existing tables.')
+            
+    def do_use(self, arg):
+        '''
+        arugment parser for switching to a database
+        sets the path to the database to be queried
+        loads tables names from database into memory.
+        '''
+        args = arg.split()
+        if args[0] in ['db', 'database']:
+            db_name = args[1]
+            db_path = data_dir / db_name
+
+            if os.path.exists(db_path):
+                self.current_db = db_path
+                self.tables = [dir.stem for dir in list(db_path.iterdir())]
+                print(f'Using database: {db_name}')
+            else:
+                print(f'Database {db_name} not found')
         else:
-            print(f'Database not set. Please set database to use to view existing tables.')
-
+            print('Unrecognized command')
 
     def do_create_table(self, arg): ## TODO add option to import csv or json from path
         '''
